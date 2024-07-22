e2fd5afd3b32a68a4d14486167fc6aa2a52055bd
M-Husaini <mohammad.husaini@iztechvalley.ps>
2024-07-22 09:52:26 +0300

    fix

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ import "dotenv/config";
 const app = express();
 const PORT = process.env.PORT;
 
+app.get("/", (req, res) => {
+  res.send("Health check!");
+});
 app.listen(PORT, () => {
   console.log(`Server running at Port :${PORT}`);
 });
