667305d2c3a46b7b42f6f75161c30ebde56a675a
M-Husaini <mohammad.husaini@iztechvalley.ps>
2024-07-22 11:27:31 +0300

    fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("Health check :3");
+  res.send("Health check problem");
 });
 app.listen(PORT, () => {
   console.log(`Server running at Port :${PORT}`);
